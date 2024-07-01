eb0f67e75dc1cb9b15e96cb54a04b9293f051f71
Cengiz48 <91730396+Cengiz48@users.noreply.github.com>
2024-07-01 17:44:32 +0300

    Add files via upload

---
+++ C#_Windows_Form_Ornek/Ornek3_ComboBox/Ornek3_ComboBox/Urun.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek3_ComboBox
+{
+    public class Urun
+    {
+        //Property tanımları
+        public int id { get; set; }
+        public string kitapAdi { get; set; }
+        public string tur { get; set; }
+        public int stokAdet { get; set; }
+        public string yazar { get; set; }
+        public string aciklama { get; set; }
+        public string pctUrunResim { get; set; }
+
+        
+        
+        public override string ToString()
+        {
+            return kitapAdi;
+        }
+
+    }
+}
