b0377237442008949911fcca3fbd05c2a5a025b7
Cengiz48 <91730396+Cengiz48@users.noreply.github.com>
2024-07-12 17:53:41 +0300

    C# Ögrenme Aşaması

---
+++ C#_Windows_Form_Ornek/Ornek5_RSS/Ornek5_RSS/Ornek5_RSS/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Ornek5_RSS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.lstGüncelHaberler = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGetir);
+            this.groupBox1.Controls.Add(this.txtUrl);
+            this.groupBox1.Location = new System.Drawing.Point(32, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1242, 56);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "URL";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.txtUrl.Location = new System.Drawing.Point(7, 20);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(1126, 23);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "https://tr.cointelegraph.com/rss/tag/bitcoin";
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnGetir.Location = new System.Drawing.Point(1140, 20);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(96, 23);
+            this.btnGetir.TabIndex = 1;
+            this.btnGetir.Text = "Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // lstGüncelHaberler
+            // 
+            this.lstGüncelHaberler.FormattingEnabled = true;
+            this.lstGüncelHaberler.ItemHeight = 17;
+            this.lstGüncelHaberler.Location = new System.Drawing.Point(6, 19);
+            this.lstGüncelHaberler.Name = "lstGüncelHaberler";
+            this.lstGüncelHaberler.Size = new System.Drawing.Size(188, 633);
+            this.lstGüncelHaberler.TabIndex = 1;
+            this.lstGüncelHaberler.SelectedIndexChanged += new System.EventHandler(this.lstGüncelHaberler_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstGüncelHaberler);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.groupBox2.Location = new System.Drawing.Point(39, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 653);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Güncel Haberler";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.webBrowser);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.groupBox3.Location = new System.Drawing.Point(260, 100);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1014, 653);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Haber İçeriği";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(3, 19);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1008, 631);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1327, 775);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Bitcoin Haber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.ListBox lstGüncelHaberler;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ C#_Windows_Form_Ornek/Ornek5_RSS/Ornek5_RSS/Ornek5_RSS/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Ornek5_RSS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            List<Haber> haberListesi = new List<Haber>();
+            XDocument xDocument = XDocument.Load(txtUrl.Text);
+            List<XElement> Rows = xDocument.Descendants("item").ToList();
+            foreach (XElement item in Rows)
+            {
+                Haber temp =new Haber();
+                temp.baslik = item.Element("title").Value;
+                temp.link = item.Element("link").Value;
+                temp.aciklama = item.Element("description").Value;
+                haberListesi.Add(temp);
+            }
+            lstGüncelHaberler.DataSource= haberListesi;
+
+            
+        }
+
+        private void lstGüncelHaberler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox secilenDeger = (ListBox)sender;
+            Haber secilenHaber = (Haber)secilenDeger.SelectedItem;
+            webBrowser.DocumentText = secilenHaber.aciklama;
+        }
+    }
+}
