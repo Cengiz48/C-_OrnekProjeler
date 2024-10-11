2038b7a4cf67b0514d894d802667f8cc6689e0d7
Cengiz48 <91730396+Cengiz48@users.noreply.github.com>
2024-10-11 23:55:19 +0300

    C# Öğrenme Aşaması

---
+++ C#_Windows_Form_Ornek/Ornek7_HavaDurumuApi/Ornek7_HavaDurumuApi/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Ornek7_HavaDurumuApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_sicaklik = new System.Windows.Forms.Label();
+            this.pct_havadurum = new System.Windows.Forms.PictureBox();
+            this.lbl_rüzgar = new System.Windows.Forms.Label();
+            this.lbl_nem = new System.Windows.Forms.Label();
+            this.lbl_rüzgaryönü = new System.Windows.Forms.Label();
+            this.lbl_tarih = new System.Windows.Forms.Label();
+            this.lbl_havanındurumu = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_havadurum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MUĞLA İÇİN HAVA DURUMU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(32, 293);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(32, 362);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(32, 293);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 19);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Rüzgar : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(32, 362);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 19);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Nem : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(32, 326);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 19);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Rüzgar Yönü : ";
+            // 
+            // lbl_sicaklik
+            // 
+            this.lbl_sicaklik.AutoSize = true;
+            this.lbl_sicaklik.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sicaklik.Location = new System.Drawing.Point(151, 149);
+            this.lbl_sicaklik.Name = "lbl_sicaklik";
+            this.lbl_sicaklik.Size = new System.Drawing.Size(142, 73);
+            this.lbl_sicaklik.TabIndex = 2;
+            this.lbl_sicaklik.Text = "36C";
+            // 
+            // pct_havadurum
+            // 
+            this.pct_havadurum.Location = new System.Drawing.Point(36, 149);
+            this.pct_havadurum.Name = "pct_havadurum";
+            this.pct_havadurum.Size = new System.Drawing.Size(109, 87);
+            this.pct_havadurum.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_havadurum.TabIndex = 3;
+            this.pct_havadurum.TabStop = false;
+            // 
+            // lbl_rüzgar
+            // 
+            this.lbl_rüzgar.AutoSize = true;
+            this.lbl_rüzgar.Location = new System.Drawing.Point(96, 293);
+            this.lbl_rüzgar.Name = "lbl_rüzgar";
+            this.lbl_rüzgar.Size = new System.Drawing.Size(41, 19);
+            this.lbl_rüzgar.TabIndex = 4;
+            this.lbl_rüzgar.Text = "35%";
+            // 
+            // lbl_nem
+            // 
+            this.lbl_nem.AutoSize = true;
+            this.lbl_nem.Location = new System.Drawing.Point(89, 362);
+            this.lbl_nem.Name = "lbl_nem";
+            this.lbl_nem.Size = new System.Drawing.Size(41, 19);
+            this.lbl_nem.TabIndex = 4;
+            this.lbl_nem.Text = "35%";
+            // 
+            // lbl_rüzgaryönü
+            // 
+            this.lbl_rüzgaryönü.AutoSize = true;
+            this.lbl_rüzgaryönü.Location = new System.Drawing.Point(137, 326);
+            this.lbl_rüzgaryönü.Name = "lbl_rüzgaryönü";
+            this.lbl_rüzgaryönü.Size = new System.Drawing.Size(18, 19);
+            this.lbl_rüzgaryönü.TabIndex = 4;
+            this.lbl_rüzgaryönü.Text = "S";
+            // 
+            // lbl_tarih
+            // 
+            this.lbl_tarih.AutoSize = true;
+            this.lbl_tarih.Location = new System.Drawing.Point(36, 88);
+            this.lbl_tarih.Name = "lbl_tarih";
+            this.lbl_tarih.Size = new System.Drawing.Size(49, 19);
+            this.lbl_tarih.TabIndex = 5;
+            this.lbl_tarih.Text = "label7";
+            // 
+            // lbl_havanındurumu
+            // 
+            this.lbl_havanındurumu.AutoSize = true;
+            this.lbl_havanındurumu.Location = new System.Drawing.Point(164, 226);
+            this.lbl_havanındurumu.Name = "lbl_havanındurumu";
+            this.lbl_havanındurumu.Size = new System.Drawing.Size(20, 19);
+            this.lbl_havanındurumu.TabIndex = 6;
+            this.lbl_havanındurumu.Text = "A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(418, 439);
+            this.Controls.Add(this.lbl_havanındurumu);
+            this.Controls.Add(this.lbl_tarih);
+            this.Controls.Add(this.lbl_rüzgaryönü);
+            this.Controls.Add(this.lbl_nem);
+            this.Controls.Add(this.lbl_rüzgar);
+            this.Controls.Add(this.pct_havadurum);
+            this.Controls.Add(this.lbl_sicaklik);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Muğla Hava Durumu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pct_havadurum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl_sicaklik;
+        private System.Windows.Forms.PictureBox pct_havadurum;
+        private System.Windows.Forms.Label lbl_rüzgar;
+        private System.Windows.Forms.Label lbl_nem;
+        private System.Windows.Forms.Label lbl_rüzgaryönü;
+        private System.Windows.Forms.Label lbl_tarih;
+        private System.Windows.Forms.Label lbl_havanındurumu;
+    }
+}
+
---
+++ C#_Windows_Form_Ornek/Ornek7_HavaDurumuApi/Ornek7_HavaDurumuApi/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Ornek7_HavaDurumuApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string resimYolu = System.IO.Path.Combine(Application.StartupPath, "resimdosyasi.jpg");
+
+                lbl_tarih.Text = DateTime.Now.ToString();
+                string api = "0d5fc9aa400ed41ba4b9420c7369c9cc";
+                string connection = "https://api.openweathermap.org/data/2.5/weather?q=mu%C4%9Fla&mode=xml&lang=tr&units=metric&appid=" + api;
+                XDocument havadurumu = XDocument.Load(connection);
+
+                var sicaklik = havadurumu.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+                //var yagis = havadurumu.Descendants("").ElementAt(0).Attribute("").Value;
+                var nem = havadurumu.Descendants("humidity").ElementAt(0).Attribute("value").Value;
+                var rüzgar = havadurumu.Descendants("speed").ElementAt(0).Attribute("value").Value;
+                var rüzgaryön = havadurumu.Descendants("direction").ElementAt(0).Attribute("name").Value;
+                var havanındurumu = havadurumu.Descendants("weather").ElementAt(0).Attribute("value").Value;
+
+                lbl_rüzgar.Text = rüzgar + "" + "m/s";
+                lbl_nem.Text = nem + "" + "%";
+                lbl_rüzgaryönü.Text = rüzgaryön;
+                lbl_havanındurumu.Text = havanındurumu.ToUpper();
+                lbl_sicaklik.Text = sicaklik + "°C";
+
+
+                if (havanındurumu == "açık")
+                {
+                    pct_havadurum.ImageLocation = System.IO.Path.Combine(Application.StartupPath, "güneşli.png");
+                }
+                if (havanındurumu == "parçalı bulutlu")
+                {
+                    pct_havadurum.ImageLocation = System.IO.Path.Combine(Application.StartupPath, "parçalıbulutlu.png");
+                }
+                if (havanındurumu == "kapalı")
+                {
+                    pct_havadurum.ImageLocation = System.IO.Path.Combine(Application.StartupPath, "bulutlu.png");
+                }
+                if (havanındurumu == "yağmurlu")
+                {
+                    pct_havadurum.ImageLocation = System.IO.Path.Combine(Application.StartupPath, "yağmurlu.png");
+                }
+
+
+            }
+            catch 
+            {
+           
+                MessageBox.Show("Hata Oluştu","Hata",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+               
+            
+
+
+
+        }
+    }
+}
