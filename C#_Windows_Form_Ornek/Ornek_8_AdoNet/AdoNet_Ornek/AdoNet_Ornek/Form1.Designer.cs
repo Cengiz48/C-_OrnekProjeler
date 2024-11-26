45d62311e29a5faa31fa935b9efdcb2bf2e7e333
Cengiz48 <91730396+Cengiz48@users.noreply.github.com>
2024-11-26 13:32:11 +0300

    C# Öğrenme Aşaması

---
+++ C#_Windows_Form_Ornek/Ornek_8_AdoNet/AdoNet_Ornek/AdoNet_Ornek/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace AdoNet_Ornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Ad = new System.Windows.Forms.TextBox();
+            this.txt_Soyad = new System.Windows.Forms.TextBox();
+            this.txt_Telefon = new System.Windows.Forms.TextBox();
+            this.dtp_Dogum = new System.Windows.Forms.DateTimePicker();
+            this.btn_Ekle = new System.Windows.Forms.Button();
+            this.btn_Sil = new System.Windows.Forms.Button();
+            this.btn_Guncelle = new System.Windows.Forms.Button();
+            this.dgw_Tablo = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Id = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_Tablo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 112);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Doğum Tarihi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 141);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Telefon";
+            // 
+            // txt_Ad
+            // 
+            this.txt_Ad.Location = new System.Drawing.Point(119, 42);
+            this.txt_Ad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_Ad.Name = "txt_Ad";
+            this.txt_Ad.Size = new System.Drawing.Size(213, 22);
+            this.txt_Ad.TabIndex = 1;
+            // 
+            // txt_Soyad
+            // 
+            this.txt_Soyad.Location = new System.Drawing.Point(119, 78);
+            this.txt_Soyad.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Soyad.Name = "txt_Soyad";
+            this.txt_Soyad.Size = new System.Drawing.Size(213, 22);
+            this.txt_Soyad.TabIndex = 1;
+            // 
+            // txt_Telefon
+            // 
+            this.txt_Telefon.Location = new System.Drawing.Point(119, 141);
+            this.txt_Telefon.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Telefon.Name = "txt_Telefon";
+            this.txt_Telefon.Size = new System.Drawing.Size(213, 22);
+            this.txt_Telefon.TabIndex = 1;
+            // 
+            // dtp_Dogum
+            // 
+            this.dtp_Dogum.Location = new System.Drawing.Point(119, 112);
+            this.dtp_Dogum.Name = "dtp_Dogum";
+            this.dtp_Dogum.Size = new System.Drawing.Size(213, 22);
+            this.dtp_Dogum.TabIndex = 2;
+            // 
+            // btn_Ekle
+            // 
+            this.btn_Ekle.Location = new System.Drawing.Point(509, 17);
+            this.btn_Ekle.Name = "btn_Ekle";
+            this.btn_Ekle.Size = new System.Drawing.Size(108, 41);
+            this.btn_Ekle.TabIndex = 3;
+            this.btn_Ekle.Text = "Ekle";
+            this.btn_Ekle.UseVisualStyleBackColor = true;
+            this.btn_Ekle.Click += new System.EventHandler(this.btn_Ekle_Click);
+            // 
+            // btn_Sil
+            // 
+            this.btn_Sil.Location = new System.Drawing.Point(509, 71);
+            this.btn_Sil.Name = "btn_Sil";
+            this.btn_Sil.Size = new System.Drawing.Size(108, 41);
+            this.btn_Sil.TabIndex = 3;
+            this.btn_Sil.Text = "Sil";
+            this.btn_Sil.UseVisualStyleBackColor = true;
+            this.btn_Sil.Click += new System.EventHandler(this.btn_Sil_Click);
+            // 
+            // btn_Guncelle
+            // 
+            this.btn_Guncelle.Location = new System.Drawing.Point(509, 129);
+            this.btn_Guncelle.Name = "btn_Guncelle";
+            this.btn_Guncelle.Size = new System.Drawing.Size(108, 41);
+            this.btn_Guncelle.TabIndex = 3;
+            this.btn_Guncelle.Text = "Güncelle";
+            this.btn_Guncelle.UseVisualStyleBackColor = true;
+            this.btn_Guncelle.Click += new System.EventHandler(this.btn_Guncelle_Click);
+            // 
+            // dgw_Tablo
+            // 
+            this.dgw_Tablo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgw_Tablo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_Tablo.Location = new System.Drawing.Point(13, 225);
+            this.dgw_Tablo.Name = "dgw_Tablo";
+            this.dgw_Tablo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgw_Tablo.Size = new System.Drawing.Size(604, 150);
+            this.dgw_Tablo.TabIndex = 4;
+            this.dgw_Tablo.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgw_Tablo_CellEnter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Id";
+            // 
+            // txt_Id
+            // 
+            this.txt_Id.Enabled = false;
+            this.txt_Id.Location = new System.Drawing.Point(119, 13);
+            this.txt_Id.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Id.Name = "txt_Id";
+            this.txt_Id.Size = new System.Drawing.Size(213, 22);
+            this.txt_Id.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 398);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgw_Tablo);
+            this.Controls.Add(this.btn_Guncelle);
+            this.Controls.Add(this.btn_Sil);
+            this.Controls.Add(this.btn_Ekle);
+            this.Controls.Add(this.dtp_Dogum);
+            this.Controls.Add(this.txt_Telefon);
+            this.Controls.Add(this.txt_Soyad);
+            this.Controls.Add(this.txt_Id);
+            this.Controls.Add(this.txt_Ad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_Tablo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Ad;
+        private System.Windows.Forms.TextBox txt_Soyad;
+        private System.Windows.Forms.TextBox txt_Telefon;
+        private System.Windows.Forms.DateTimePicker dtp_Dogum;
+        private System.Windows.Forms.Button btn_Ekle;
+        private System.Windows.Forms.Button btn_Sil;
+        private System.Windows.Forms.Button btn_Guncelle;
+        private System.Windows.Forms.DataGridView dgw_Tablo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Id;
+    }
+}
+
---
+++ C#_Windows_Form_Ornek/Ornek_8_AdoNet/AdoNet_Ornek/AdoNet_Ornek/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNet_Ornek
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void MusteriGetir()
+        {
+            baglanti= new SqlConnection("server=localhost\\SQLEXPRESS;Initial Catalog=MusteriTakip;Integrated Security=True;");
+            baglanti.Open();
+            da = new SqlDataAdapter("SELECT * FROM musteri", baglanti);
+            DataTable tablo= new DataTable();
+            da.Fill(tablo);
+            dgw_Tablo.DataSource = tablo;
+            baglanti.Close();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MusteriGetir();
+
+        }
+
+        private void dgw_Tablo_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_Id.Text = dgw_Tablo.CurrentRow.Cells[0].Value.ToString();
+            txt_Ad.Text = dgw_Tablo.CurrentRow.Cells[1].Value.ToString();
+            txt_Soyad.Text = dgw_Tablo.CurrentRow.Cells[2].Value.ToString();
+            dtp_Dogum.Text = dgw_Tablo.CurrentRow.Cells[3].Value.ToString();
+            txt_Telefon.Text = dgw_Tablo.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void btn_Ekle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO musteri(Ad,Soyad,Dogum_Tarihi,Telefon) VALUES (@Ad,@Soyad,@Dogum_Tarihi,@Telefon)";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Ad",txt_Ad.Text);
+            komut.Parameters.AddWithValue("@Soyad",txt_Soyad.Text);
+            komut.Parameters.AddWithValue("@Dogum_Tarihi",dtp_Dogum.Value);
+            komut.Parameters.AddWithValue("@Telefon",txt_Telefon.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MusteriGetir();
+            MessageBox.Show("Başarıyla Eklendi", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void btn_Sil_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE FROM musteri WHERE Id=@Id";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("Id", Convert.ToInt32(txt_Id.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MusteriGetir();
+            MessageBox.Show("Başarıyla Silindi", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void btn_Guncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE musteri SET Ad=@Ad,Soyad=@Soyad,Dogum_Tarihi=@Dogum_Tarihi,Telefon=@Telefon where Id=@Id";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Id", Convert.ToInt32(txt_Id.Text));
+            komut.Parameters.AddWithValue("@Ad", txt_Ad.Text);
+            komut.Parameters.AddWithValue("@Soyad", txt_Soyad.Text);
+            komut.Parameters.AddWithValue("@Dogum_Tarihi", dtp_Dogum.Value);
+            komut.Parameters.AddWithValue("@Telefon", txt_Telefon.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MusteriGetir();
+            MessageBox.Show("Başarıyla Güncellendi", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
